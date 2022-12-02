57e205a5112b705b27ccf1633187108077cf56d2
moyudexiaosong <107531764+moyudexiaosong@users.noreply.github.com>
2022-12-02 15:01:40 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '09d56b5cb50940558c02895d39f6d1b5',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Notion Usage',
+  domain: 'https://www.notion.so/Notion-09d56b5cb50940558c02895d39f6d1b5',
+  author: 'Aniya',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
